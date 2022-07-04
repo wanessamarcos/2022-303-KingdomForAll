a380dde9785e05cae2ff47396abdbbdba4cbdf77
wanessamarcos <2020954170@teiacoltec.org>
2022-07-04 14:04:39 -0300

    menu 2.0

---
+++ MenuJogoof/MenuJogo/Assets/BackgroungScrooling.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroungScrooling : MonoBehaviour
+{
+
+    public MeshRenderer mr;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mr.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
